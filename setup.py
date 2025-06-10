18cb38336af495594e42ea5950cbf7c3698d4b2c
ShawnJi <826692353@qq.com>
2025-06-10 23:13:48 +0800

    python要求改为3.8

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def format_msg(text, *args, **kwargs):
 
 
 CURRENT_PYTHON = sys.version_info[:2]
-REQUIRED_PYTHON = (3, 9)
+REQUIRED_PYTHON = (3, 8)
 
 # This check and everything above must remain compatible with older Python versions
 if CURRENT_PYTHON < REQUIRED_PYTHON:
